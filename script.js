507147ab6d7386dc68fd4a20d9f724cbb3616b5f
Aamir Nathani <amirnathani9@gmail.com>
2021-08-24 15:59:33 +0530

    done

--- script.js
+++ script.js
@@ -5,23 +5,25 @@ var outputTxt = document.querySelector("#output-txt");
 var serverURL = "https://api.funtranslations.com/translate/minion.json"
 
 function getTranslatorURL(input) {
-    return serverURL + "?" + "text=" +input
+    return serverURL + "?" + "text=" + input
 }
 
 function errorHandler(error) {
     alert("Something went wrong.Plzz Try Later")
 }
 
-function clickEvent(){
-    var txtInput = inputTxt.value;
+function clickEvent() {
+    var textInput = inputTxt.value;
 
-    fetch(getTranslatorURL(txtInput))
+    fetch(getTranslatorURL(textInput))
         .then(response => response.json())
         .then(json => {
             var translatedOutput = json.contents.translator;
-            outputTxt.innerTxt =translatedOutput
+            outputTxt.innerText = translatedOutput;
         })
     .catch(errorHandler)
 }
 
 btnTranslator.addEventListener("click",clickEvent)
+
+
